49e1fab81f49efc0a7b090c280db1999e335b8f4
Brian Leonard <brian@bleonard.com>
2011-11-10 20:41:29 -0800

    test for failed login

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe "Login" do
-  it "should show draft tasks in history" do
-    user = Factory(:user, :password => "letmein")
+  before do
+    @user = Factory(:user, :password => "letmein")
+  end
   
+  it "should allow login" do
     visit '/'
-    fill_in("Email", :with => user.email)
+    fill_in("Email", :with => @user.email)
     fill_in("Password", :with => "letmein")
     click_button("Sign in")
     
     current_path.should == '/home'
     page.should have_content('Signed in successfully.')
   end
+  
+  it "should show validations" do
+    visit '/'
+    fill_in("Email", :with => @user.email)
+    fill_in("Password", :with => "wrong!")
+    click_button("Sign in")
+    
+    current_path.should == '/users/sign_in'
+    page.should have_content('Invalid email or password.')
+  end
+  
 end
