b042d0edfd18461e8362f582e136a33116cda130
Brian Leonard <brian@bleonard.com>
2011-11-11 22:43:40 -0800

    take an action to show the table content

--- spec/integration/tables_manage_spec.rb
+++ spec/integration/tables_manage_spec.rb
@@ -12,4 +12,24 @@ describe "Managing Tables" do
     page.should have_content("The page you were looking for doesn't exist (404)")
   end
   
+  it "should allow testing of the table" do
+    Table.any_instance.stubs(:fetch).returns(
+              Ruport::Data::Table.new(:data => [["eggs","5"], ["milk","3 gallons"]],
+                                      :column_names => %w[item amount])     
+              )
+    table = Factory(:table, :user => @user)
+    
+    visit table_path(table)
+    
+    page.should have_no_content("gallons")
+    page.should have_no_content("amount")
+    
+    click_link "See Table Output"
+    
+    current_path.should == table_path(table)
+    page.should have_content("3 gallons")
+    page.should have_content("eggs")
+    page.should have_content("amount")
+  end
+  
 end
