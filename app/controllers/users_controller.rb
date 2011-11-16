e2a0d6155dd59196247f90c9969e8459e41f64d8
Brian Leonard <brian@bleonard.com>
2011-11-15 18:20:54 -0800

    change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,20 @@ class UsersController < InheritedResources::Base
   filter_resource_access
   
   def update
+    params[:user][:password] = nil if params[:user][:password].blank?
+    
     update! do |success, failure|
-      success.all { redirect_to user_root_path }
+      success.all { redirect_after_update }
       failure.all { render :edit }
     end
   end
   
   protected
   def user_from_current
-    params[:id] ||= current_user.id
+    params[:id] ||= (current_user.try(:id) || 0)
+  end
+  def redirect_after_update
+    sign_in @user, :bypass => true if @user == current_user
+    redirect_to user_root_path
   end
 end
