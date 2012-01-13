285f269086ebaecd3d18023d4887c9e1f2eb6e8f
Brian Leonard <brian@bleonard.com>
2012-01-12 20:46:14 -0800

    include rake tasks in engine

--- lib/engine/engine.rb
+++ lib/engine/engine.rb
@@ -1,4 +1,10 @@
 module Daily
   class Engine < Rails::Engine
+    
+    # Load rake tasks
+    rake_tasks do
+      load File.expand_path('../../tasks/user.rake', __FILE__)
+    end
+    
   end
 end
